d15093afe54a914418570cc4c3cbd983807db9ee
unknown <raymondlevengood@gmail.com>
2020-02-21 17:37:08 -0500

    fixed bug in gamesList

--- primo-frontend/src/App.js
+++ primo-frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-        <Home />
+        {/* <Home /> */}
       <GamesList/>
       <Footer/>
     </div>
--- primo-frontend/src/Components/Games/GamesList.js
+++ primo-frontend/src/Components/Games/GamesList.js
@@ -6,6 +6,12 @@ function GamesList() {
     return (
         <>
             <div className="gamesListBody">
+                <div className="gamesListBodySubCata">
+                    <p>Catagories</p>
+                    <div className="gamesListCatagories">
+                    <h5>stuff goes heres</h5>
+                    </div>
+                </div>
                 <div className="gamesListBodySearch">
                     <form className="gamesListGameSearch">
                         <input type="text" placeholder="Search for a game here..." name="search" className="gamesListGameSearchbar"/>
@@ -17,11 +23,6 @@ function GamesList() {
                 </div>
                 <div className="gameListBodyBlog">
                     <h1>A, B, C, D, etc, etc</h1>
-                </div>
-            </div>
-            <div className="gamesListCatagoriesDiv">
-                <p>Catagories</p>
-                <div className="gamesListCatagories">
                 </div>
             </div>
         </>
